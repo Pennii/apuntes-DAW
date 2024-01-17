6731f57641fed2da1a4fffe1562034cd0bb212b5
penni <matiasspennino@gmail.com>
2024-01-17 14:07:30 +0100

    mezcla

---
+++ Programacion/Unidad4/src/unidad4/MezclaCadenas.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package unidad4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author maniana
+ */
+public class MezclaCadenas {
+
+    public static void main(String[] args) {
+        String cadena1, cadena2, mezcla[];
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("ingresa la primera cadena");
+        cadena1 = teclado.nextLine();
+        System.out.println("Ingresa la segunda");
+        cadena2 = teclado.nextLine();
+
+        
+    }
+}

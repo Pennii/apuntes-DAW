941d8a5bfa31f62fd78b80a7e483677796a58cb9
penni <matiasspennino@gmail.com>
2024-03-22 10:39:44 +0100

    probando git en netbeans

--- Programacion/Unidad7/src/tarea/Tienda.java
+++ Programacion/Unidad7/src/tarea/Tienda.java
@@ -6,15 +6,10 @@ package tarea;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;

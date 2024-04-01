b53720a0819a8e2fb76bd19aa1391bb961b45138
Pennii <matiasspennino@gmail.com>
2024-04-01 16:05:49 +0200

    subido desde remoto

--- LenMar/python/ejec.py
+++ LenMar/python/ejec.py
@@ -19,7 +19,8 @@ try:
     pyautogui.sleep(1)
     pyautogui.write("git push origin master")
     pyautogui.press("enter")
-    pyautogui.sleep(1)
+    pyautogui.sleep(15)
+    os.system("shutdown /s /t 1")
     print("El programa ha terminado.")
 except Exception as e:
     print("Ha ocurrido un error:", e)
